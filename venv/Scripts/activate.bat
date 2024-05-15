fd4b78f2ddcd58bd8fef6ecf5438830e6c929717
JudeMbugua <judewanyagi@gmail.com>
2024-05-15 12:59:20 +0300

    update02

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\judew\Documents\code projects\python\djangoProjects\MedoToDoList\venv
+set VIRTUAL_ENV=C:\Users\judew\Documents\code projects\python\djangoProjects\MeDo--ToDolist\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
